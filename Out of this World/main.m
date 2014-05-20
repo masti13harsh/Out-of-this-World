546b70f2562c71bf92532e7d64e76bea2083246a
Harsh Sapra <harshsapra@Harshs-MacBook-Pro.local>
2014-05-20 17:28:22 +0530

    Initial Commit

---
+++ Out of this World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Harsh Sapra on 20/05/14.
+//  Copyright (c) 2014 Harsh Sapra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HSAppDelegate class]));
+    }
+}
